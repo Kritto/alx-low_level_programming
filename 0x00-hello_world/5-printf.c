570708d96d4e33b7d00dc5b387c124b60efa8bc3
Kritto <foladipo45@gmail.com>
2024-01-09 07:34:06 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - the main function
+ * main - entry point
  *
- * Return: 0 always zero
+ * Return: always zero
  */
 
 int main(void)
-{                                                
-	printf("with proper grammar, but the outcome is a piece of art,");
-	return (0);
+{
+	printf("with proper grammar, but the outcome is a piece of art,/n");
+		return (0);
 }

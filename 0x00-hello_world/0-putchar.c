f4283572e9e7fc8cc27a21c3be4dca1e4c4dbf51
kritto <foladipo45@gmail.com>
2024-01-13 04:21:27 +0100

    putchar

---
+++ 0x00-hello_world/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * Main - main funciton
+ * Return: returns
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x00-hello_world/test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: zero
+ */
+int main(void)
+{
+	char var[100];
+	printf("What is your name?\n");
+	scanf("%c");
+	printf("Hello World!\n");
+	return (0);
+}

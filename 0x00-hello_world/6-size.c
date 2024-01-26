dceac0642ea9a515b9fd71f05779ca590d7e5a0a
kritto <foladipo45@gmail.com>
2024-01-26 16:02:18 +0100

    data sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,6 +6,11 @@
 
 int main(void)
 {
+	int int_num = 0;
+	char ch = ' ';
+	float float_num = 0.0f;
+	double dbl_num = 0.0;
+
 	printf("size of a char: %ld byte(s)\n", sizeof(char));
 	printf("Size of an int: %ld byte(s)\n", sizeof(int));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
